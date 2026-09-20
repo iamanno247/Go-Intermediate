75a16b11104ec98d47137aa187c66bf14e45f25c
ANNO <iamanno247@gmail.com>
2026-09-20 13:33:51 +0600

    erroesAsValues

--- main.go
+++ main.go
@@ -1,38 +1,25 @@
 package main
-import ("bufio"; "fmt"; "os"; "strconv")
+import ("bufio"; "errors"; "fmt"; "os"; "strconv")
 
-// type Shape interface { ... }
-type Shape interface {
-	Area() float64
-}
-// type Circle struct { ... }
-type Circle struct {
-	Radius float64
-}
-type Square struct {
-	Side float64
-}
-func (c Circle) Area() float64 {
-	return 3.14 * c.Radius * c.Radius
-}
-func (s Square) Area() float64 {
-	return s.Side * s.Side
+func validateAge(s string) (int, error) {
+    // implement
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			return 0, fmt.Errorf("parse: %v", err)
+		} else if n < 0 {
+			return 0, errors.New("negative")
+		} else {
+    return n, nil
+	  }
 }
 
 func main() {
     sc := bufio.NewScanner(os.Stdin)
-    sc.Scan(); kind := sc.Text()
-    sc.Scan(); dim, _ := strconv.ParseFloat(sc.Text(), 64)
-    var s interface{ Area() float64 }
-    _ = kind; _ = dim
-
-		switch kind {
-		case "circle":
-			s = Circle{Radius: dim}
-		case "square":
-			s = Square{Side: dim}
-		}
-
-    if s != nil { fmt.Printf("%.2f\n", s.Area())
-	}
+    sc.Scan()
+    age, err := validateAge(sc.Text())
+    if err != nil {
+        fmt.Printf("error: %s\n", err.Error())
+    } else {
+        fmt.Printf("age: %d\n", age)
+    }
 }
